530a65717c993426393c06de8f8172c6d4bf6a88
Swerwe <92936992+Swerwe@users.noreply.github.com>
2022-09-21 23:24:10 +0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:'/spotify-clone-vueJS',
+  publicPath:'/spotify-clone-vueJS/',
   devServer:{
     proxy:'https://cse.google.com'
   }
